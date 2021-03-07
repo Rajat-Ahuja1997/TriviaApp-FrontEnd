915d365d40c77bbcc0e8661c1833e2f651b3b8fe
Rajat Ahuja <Rajat_Ahuja@utexas.edu>
2021-03-06 19:08:46 -0600

    Fixing CSS to use columns/rows

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,19 +5,21 @@ import './Form.css';
 class Form extends React.Component {
     render() {
         return (
-            <form onSubmit={this.props.submit}>
-                <div id="inputForm">
-                    <input 
-                        pattern="[\d]{10}" 
-                        placeholder="Enter 10-digit number" 
-                        value={this.props.value} 
-                        onChange={this.props.change} 
-                        title="Phone number should match a 10-digit format (ex: 1234567890)"
-                        id="inputBar"
-                    />
-                    <button id="inputButton" type="button" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+            <div>
+                <form onSubmit={this.props.submit} id="form1">
+                    <div id="inputForm">
+                        <input 
+                            pattern="[\d]{10}" 
+                            placeholder="Enter 10-digit number" 
+                            value={this.props.value} 
+                            onChange={this.props.change} 
+                            title="Phone number should match a 10-digit format (ex: 1234567890)"
+                            id="inputBar"
+                        />
+                    </div>
+                </form>
+                <button id="inputButton" type="submit" class="btn btn-primary" form="form1">Submit</button>
+            </div>
         );
     }
 }
